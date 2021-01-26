c8dd85382475251eea96466efbe9a1b6a566e67d
isyel <isykaal4saviour@gmail.com>
2021-01-26 07:49:14 +0100

    🔰 First commit - Created usable components(card, input, colors) - Created game screens - Created logic for COM guess of numbers - Added logic to help computer guess better

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+import GameOverScreen from "./components/screens/GameOverScreen";
+import GameScreen from "./components/screens/GameScreen";
+import StartGameScreen from "./components/screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [userNumber, setUserNumber] = useState();
+	const [noOfGuessRounds, setNoOfGuessRounds] = useState(0);
+
+	const startGameHandler = (selectedNumber) => {
+		setUserNumber(selectedNumber);
+		setNoOfGuessRounds(0);
+	};
+
+	const gameOverHandler = (numberofRounds) => {
+		setNoOfGuessRounds(numberofRounds);
+	};
+
+	const configureNewGameHandler = () => {
+		setNoOfGuessRounds(0);
+		setUserNumber(null);
+	};
+
+	let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+	if (userNumber && noOfGuessRounds <= 0) {
+		content = (
+			<GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+		);
+	} else if (noOfGuessRounds > 0) {
+		content = (
+			<GameOverScreen
+				noOfRoundsTaken={noOfGuessRounds}
+				userNumber={userNumber}
+				onRestart={configureNewGameHandler}
+			/>
+		);
+	}
+
+	return (
+		<View style={styles.screen}>
+			<Header title="Guess a Number" />
+			{content}
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	screen: {
+		flex: 1,
+	},
 });
